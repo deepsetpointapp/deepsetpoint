44239b0d8d8a3e04caae395cf0170a5e5920c978
Andre Afrika <ndrew@Andres-Mac-mini.local>
2026-02-13 02:46:12 +0100

    Fix svelte.config.js syntax

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
-// svelte.config.js
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
